5f0596af60ef308b1d88ebbadc6b12a4b02acbb1
soenneker <jake@soenneker.com>
2026-09-13 18:02:43 -0500

    misc

--- src/Soenneker.Utils.CancellationScopes/CancellationGeneration.cs
+++ src/Soenneker.Utils.CancellationScopes/CancellationGeneration.cs
@@ -5,6 +5,7 @@ namespace Soenneker.Utils.CancellationScopes;
 internal sealed class CancellationGeneration(CancellationTokenSource? source)
 {
     internal readonly CancellationTokenSource? Source = source;
+
     // Cache before publication: Source.Token can throw after concurrent disposal,
     // but a previously obtained token remains readable.
     internal readonly CancellationToken Token = source?.Token ?? default;
